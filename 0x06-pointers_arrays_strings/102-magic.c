f269c7a4530dcbcc2b50ad61b7e6d1719bbeee33
ray1913 <okebugwudaniel@gmail.com>
2022-10-27 08:25:08 -0700

    update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,17 +8,16 @@ int *p;
 
 a[2] = 1024;
 p = &n;
- /*
-  * write your line of code here...
-  * Remember:
-  * - c ain't that hrad tho
-  * - dont modify the p
-  * - statement allowed is one
-  * - dont use any code or write any except these
-  * - you are not allowed to code anything else than this line of code
-  */
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
 *(p + 5) = 98;
-/* ...so that this prints 98\n */
+  /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
